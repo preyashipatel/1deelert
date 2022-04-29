447ccfd8b91ad42df25bd5f4d6cbf5385dbf491c
preyashipatel <preyashi@xhtmljunkies.com>
2022-04-29 04:03:23 +0000

    “Message”

--- app/code/Elsnertech/Customisation/Helper/Data.php
+++ app/code/Elsnertech/Customisation/Helper/Data.php
@@ -1,8 +1,9 @@
 <?php
 namespace Elsnertech\Customisation\Helper;
  
- use Magento\Framework\App\Helper\Context;
+use Magento\Framework\App\Helper\Context;
 use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\Registry;
 
 class Data extends \Magento\Framework\App\Helper\AbstractHelper
 {
@@ -11,10 +12,12 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
     public function __construct(
         Context $context,
         ScopeConfigInterface $scopeConfig,
-        \Magento\Store\Model\StoreManagerInterface $storeManager
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        Registry $registry
     ) {
         $this->_scopeConfig = $scopeConfig;
         $this->_storeManager=$storeManager;
+        $this->_registry = $registry;
         parent::__construct($context);
     }
     
@@ -26,7 +29,10 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
         return $this->_storeManager->getStore()->getBaseUrl();
     }
     public function getMediaUrl() {
-
         return $this->_storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA);
     }
+
+    public function getCurrentProduct() {        
+        return $this->_registry->registry('current_product')->getsku();
+    } 
 }

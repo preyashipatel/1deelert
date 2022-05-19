c24800d92fce4632c62d05b9bc61b429144ae718
Preyashipatel <preyashi@xhtmljunkies.com>
2022-05-19 05:44:31 +0000

    Custom pages

--- app/code/Smartwave/Filterproducts/Block/Home/LatestList.php
+++ app/code/Smartwave/Filterproducts/Block/Home/LatestList.php
@@ -85,4 +85,42 @@ class LatestList extends \Magento\Catalog\Block\Product\ListProduct {
             $limit = 10;
         return $limit;
     }
+
+    protected function _prepareLayout()
+    {
+        parent::_prepareLayout();
+        $this->pageConfig->getTitle()->set(__('products'));
+        if ($this->_getProductCollection()) {
+        $toolbar = $this->getLayout()
+                   ->createBlock(
+                'Magento\Catalog\Block\Product\ProductList\Toolbar',
+                'product_list_toolbar'
+                )
+                ->setTemplate('Magento_Catalog::product/list/toolbar.phtml')
+                ->setCollection($this->_getProductCollection());
+
+            $pager = $this->getLayout()->createBlock(
+                'Magento\Theme\Block\Html\Pager',
+                'AllProduct.product.pager'
+            )->setAvailableLimit(array(3=>3,6=>6,9=>9))->setShowPerPage(true)->setCollection(
+                $this->_getProductCollection()
+            );
+            $this->setChild('pager', $pager);
+            $this->setChild('toolbar', $toolbar);
+            $this->_getProductCollection()->load();
+        }
+        return $this;
+    }
+    public function getPagerHtml()
+    {
+        return $this->getChildHtml('pager');
+    }
+    public function getToolbarHtml()
+    {
+        return $this->getChildHtml('toolbar');
+    }
+    public function getMode()
+    {
+        return $this->getChildBlock('toolbar')->getCurrentMode();
+    }
 }

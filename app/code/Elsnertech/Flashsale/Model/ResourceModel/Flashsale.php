61a116d32f65ead344e17099d65004c1002c5475
preyashipatel <preyashi@xhtmljunkies.com>
2022-05-24 05:19:34 +0000

    Bug Fix

--- app/code/Elsnertech/Flashsale/Model/ResourceModel/Flashsale.php
+++ app/code/Elsnertech/Flashsale/Model/ResourceModel/Flashsale.php
@@ -9,35 +9,11 @@ use Magento\Framework\Model\ResourceModel\Db\Context;
 
 class Flashsale extends AbstractDb
 {
-    public const TABLE_NAME = 'flashsale';
-    /**
-     * Undocumented variable
-     *
-     * @var [type]
-     */
+    const TABLE_NAME = 'flashsale';
     protected $flashsaleProductTable = null;
-    /**
-     * Undocumented variable
-     *
-     * @var [type]
-     */
     protected $eventManager = null;
-    /**
-     * Undocumented variable
-     *
-     * @var [type]
-     */
     protected $logger;
 
-    /**
-     * Comment of __construct function
-     *
-     * @param Context $context
-     * @param ManagerInterface $eventManager
-     * @param \Psr\Log\LoggerInterface $logger
-     * @param \Elsnertech\Flashsale\Model\FlashsaleProduct $flashsaleProductFactory
-     * @param [type] $connectionName
-     */
     public function __construct(
         Context $context,
         ManagerInterface $eventManager,
@@ -51,11 +27,8 @@ class Flashsale extends AbstractDb
 
         $this->eventManager = $eventManager;
     }
-    /**
-     * Comment of _construct function
-     *
-     * @return void
-     */
+
+
     protected function _construct()
     {
         $this->_init(static::TABLE_NAME, FlashsaleInterface::ID);
@@ -74,16 +47,13 @@ class Flashsale extends AbstractDb
         return parent::_afterSave($object);
     }
 
-    /**
-     * Comment of saveProducts function
-     *
-     * @param AbstractModel $flashsale
-     * @return void
-     */
     protected function saveProducts(AbstractModel $flashsale)
     {
+
+
         $id = $flashsale->getId();
         $product_val = $flashsale->getProductVal();
+        // print_r($flashsale->getProductSelectedValue());exit;
         if ($product_val != 0) {
             /**
              * new flashsale-product relationships
@@ -99,10 +69,15 @@ class Flashsale extends AbstractDb
                 $flashsaleProductCollection->walk('delete');
                 return $this;
             }
+
+
+
             $flashsaleProductModel = $this->flashsaleProductFactory;
             $flashsaleProductCollection = $flashsaleProductModel->getCollection()
                         ->addFieldToFilter('flashsale_id', ['eq' => $id]);
             $flashsaleProductCollection->walk('delete');
+
+
             foreach ($products as $key => $data) {
                 $flashsaleProductModel->setData(
                     [
--- app/code/Elsnertech/Homeslider/Model/ResourceModel/Homeslider.php
+++ app/code/Elsnertech/Homeslider/Model/ResourceModel/Homeslider.php
@@ -4,26 +4,11 @@ namespace Elsnertech\Homeslider\Model\ResourceModel;
 
 class Homeslider extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
 {
-    /**
-     * $_date variable
-     *
-     * @var string
-     */
-    protected $C = 'id';
-    /**
-     * $_date variable
-     *
-     * @var [type]
-     */
+    
+    protected $_idFieldName = 'id';
+    
     protected $_date;
 
-    /**
-     * Comment of __construct function
-     *
-     * @param \Magento\Framework\Model\ResourceModel\Db\Context $context
-     * @param \Magento\Framework\Stdlib\DateTime\DateTime $date
-     * @param [type] $resourcePrefix
-     */
     public function __construct(
         \Magento\Framework\Model\ResourceModel\Db\Context $context,
         \Magento\Framework\Stdlib\DateTime\DateTime $date,
@@ -33,11 +18,6 @@ class Homeslider extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
         $this->_date = $date;
     }
 
-    /**
-     * Comment of _construct function
-     *
-     * @return void
-     */
     protected function _construct()
     {
         $this->_init('homeslider', 'id');
